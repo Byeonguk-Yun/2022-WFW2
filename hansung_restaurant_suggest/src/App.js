ba26627ec4b8b416ddafc8cef9666c6a4c57def8
YuHG <hghgy@hansung.ac.kr>
2022-11-21 23:49:27 +0900

    slide 컴포넌트

--- hansung_restaurant_suggest/src/App.js
+++ hansung_restaurant_suggest/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import HeaderMenu from './Header';
+import SubwayAudio from './components/Subway';
+import AutoSlider from './components/Slider';
+import Carousel from './components/Carousel';
+import SubwaySlider from './components/SubwaySlider';
 
 function App() {
-  return (
-    <div>
+  // return (
+  //   <div>
       
-    </div>
+  //   </div>
+  // );
+
+  return (
+    <Routes>
+      <Route path="/" element={<HeaderMenu />}/>
+      <Route path="/subway" element={<SubwayAudio />}/>
+      <Route path="/slider" element={<AutoSlider />}/>
+      <Route path="/carousel" element={<Carousel />}/>
+      <Route path="/subwayslider" element={<SubwaySlider />}/>
+    </Routes>
   );
 }
 
