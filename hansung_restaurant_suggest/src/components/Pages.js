6e30547c2ceff0432d16c3748fff277ef5d57afc
Byeonguk Yun <ybw595@gmail.com>
2022-11-25 23:07:37 +0900

    first routing

---
+++ hansung_restaurant_suggest/src/components/Pages.js
@@ -0,0 +1,38 @@
+export const Korean = () => (
+    <div>
+        <br></br><br></br><br></br><br></br>
+        <h2>한식</h2>
+        <img src="../img/rImg/부부김밥.png" alt="이미지 준비중"></img>
+    </div>
+);
+
+export const Chinese = () => (
+    <div>
+        <br></br><br></br><br></br><br></br>
+        <h2>중식</h2>
+        <img src="../img/rImg/송림원.png" alt="이미지 준비중"></img>
+    </div>
+);
+
+export const Japanese = () => (
+    <div>
+        <br></br><br></br><br></br><br></br>
+        <h2>일식</h2>
+        <img src="../img/rImg/정돈 대학로본점.jpg" alt="이미지 준비중"></img>
+    </div>
+);
+
+export const Western = () => (
+    <div>
+        <br></br><br></br><br></br><br></br>
+        <h2>양식</h2>
+        <img src="../img/rImg/부부셰프.jpg" alt="이미지 준비중"></img>
+    </div>
+);
+
+export const Dessert = () => (
+    <div><br></br><br></br><br></br><br></br>
+        <h2>디저트 / 카페</h2>
+        <img src="../img/rImg/카페다오.png" alt="이미지 준비중"></img>
+    </div>
+);
