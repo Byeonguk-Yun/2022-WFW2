3df1dfe00470182c96efd6fc17056fe6d5a26573
sy_01 <92142899+Yangseyeong@users.noreply.github.com>
2022-11-26 11:57:05 +0900

    searchcomponent

---
+++ hansung_restaurant_suggest/src/searchComponent.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import './App.css';
+import Restaurant from './restaurant-json';
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state= {
+      search:null
+    };
+  }
+
+    searchSpace = (event) => {
+      let keyword = event.target.value;
+      this.setState({search:keyword})
+    }
+
+
+  render() {
+    const styleInfo = {
+      paddingRight : '10px'
+    }
+
+    const elementStyle = {
+      border : 'solid',
+      borderRadius: '10px',
+      position : 'relative',
+      left : '10vh',
+      height:'3vh',
+      width:'20vh',
+      marginTop:'5vh',
+      marginBottom:'10vh'
+    }
+
+
+
+    const items = Information.filter((data) => {
+      if(this.state.search == null)
+        return data
+      
+      else if (data.rlmg.toLowerCase().includes(this.state.search.toLowerCase()) || data.img.toLowerCase().includes(this.state.search.toLowerCase()))
+        return data
+    }
+  ).map(data => {
+    return (
+      <div>
+        <ul>
+          <li style={{position:'relative', left : '10vh'}}>
+            <span style = {styleInfo}>{data.rlmg}</span>
+            <span style = {styleInfo}>{data.img}</span>
+          </li>
+        </ul>
+      </div>
+    )
+  })
+
+    return (
+      <div>
+      <input type="text" placeholder="검색어를 입력해주세요" style = {elementStyle} onChange={(e)=> this.searchSpace(e)} />
+      {items}
+      </div> 
+    )
+
+  }
+}
+  
+  export default App;
